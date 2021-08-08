b90083a0096f486463c8f0d9be4457c83e7a8309
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-08-08 13:43:35 +0530

    Add files via upload

---
+++ Amanda/modules/about.py
@@ -0,0 +1,14 @@
+__help__ = """
+** Amanda - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer [𝕿𝖍𝖆𝖗𝖚𝖐 𝕽𝖊𝖓𝖚𝖏𝖆](https://t.me/TharukRenuja)
+"""
+__mod_name__ = "About"
